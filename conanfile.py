c73c973a8afb8761597942c9debca31ad4b0bdbf
Mingyi <mingyi.z@outlook.com>
2018-11-26 11:12:40 +0800

    msvc ok

--- conanfile.py
+++ conanfile.py
@@ -1,9 +1,17 @@
-from conans import ConanFile, CMake, tools
+from conans import ConanFile, CMake, tools,Meson
 import os
+
+def to_unicode(filename):
+    f = open(filename, mode='r', encoding='UTF-8')
+    content = f.read()
+    f.close()
+    f = open(filename,'w',encoding='UTF-16')
+    f.write(content)
+    f.close()
 
 class GrapheneConan(ConanFile):
     name = "graphene"
-    version = "1.4.0"
+    version = "1.8.2"
     description = "A thin layer of graphic data types"
     url = "https://github.com/conanos/graphene"
     homepage = "https://github.com/conanos/graphene"
@@ -12,7 +20,7 @@ class GrapheneConan(ConanFile):
     options = {"shared": [True, False]}
     default_options = "shared=True"
     generators = "cmake"
-    requires = "glib/2.58.0@conanos/dev"
+    requires = "glib/2.58.1@conanos/stable"
 
     source_subfolder = "source_subfolder"
 
@@ -20,8 +28,24 @@ class GrapheneConan(ConanFile):
         url_ = 'https://github.com/ebassi/graphene/archive/{version}.tar.gz'
         tools.get(url_.format(version=self.version))
         os.rename("%s-%s"%(self.name, self.version), self.source_subfolder)
-        
+        if self.settings.compiler == 'Visual Studio':
+            to_unicode(os.path.join(self.source_subfolder,'src/graphene-euler.c'))
+
     def build(self):
+        defs = {'prefix':self.package_folder}
+
+
+        meson = Meson(self)
+        meson.configure( defs=defs,
+            source_dir=self.source_subfolder,
+            build_dir='_build')
+        meson.build()
+
+        self.run('ninja -C {0} install'.format(meson.build_dir))
+        
+
+
+    def gcc_build(self):
         with tools.chdir(self.source_subfolder):
             _args = ['--prefix=%s/builddir'%(os.getcwd()), '--libdir=%s/builddir/lib'%(os.getcwd()), '--disable-maintainer-mode',
                      '--disable-silent-rules', '--disable-arm-neon']
