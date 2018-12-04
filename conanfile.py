d14fb8b5af72a17f054ab9cede6446de584cd41d
cppbitman <daihongjun@kedacom.com>
2018-12-04 19:49:12 +0800

    windows build done

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,5 @@
 from conans import ConanFile, CMake, tools,Meson
+from conanos.build import config_scheme
 import os
 
 def to_unicode(filename):
@@ -16,64 +17,82 @@ class GrapheneConan(ConanFile):
     url = "https://github.com/conanos/graphene"
     homepage = "https://github.com/conanos/graphene"
     license = "MIT"
+    generators = "gcc","visual_studio"
+    patch = "windows-warning-c4244.patch"
+    exports = [patch]
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False]}
-    default_options = "shared=True"
-    generators = "cmake"
+    options = {
+        "shared": [True, False],
+        'fPIC': [True, False]
+    }
+    default_options = { 'shared': False, 'fPIC': True }
     requires = "glib/2.58.1@conanos/stable"
 
-    source_subfolder = "source_subfolder"
+    _source_subfolder = "source_subfolder"
+    _build_subfolder = "build_subfolder"
 
-    def is_msvc(self):
-        return  self.settings.compiler == 'Visual Studio'
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+        config_scheme(self)
+    
+    def configure(self):
+        del self.settings.compiler.libcxx
+
+    #def is_msvc(self):
+    #    return  self.settings.compiler == 'Visual Studio'
 
 
+    #def build_requirements(self):
+    #    if self.is_msvc:
+    #        #self.build_requires("cygwin_installer/2.9.0@bincrafters/stable")
+    #        self.build_requires("msys2_installer/20161025@bincrafters/stable")
+
+    #def requirements(self):
+    #    assert(0)
     def build_requirements(self):
-        if self.is_msvc:
-            #self.build_requires("cygwin_installer/2.9.0@bincrafters/stable")
-            self.build_requires("msys2_installer/20161025@bincrafters/stable")
-
-    def requirements(self):
-        assert(0)
+        self.build_requires("libffi/3.299999@conanos/stable")
 
     def source(self):
         url_ = 'https://github.com/ebassi/graphene/archive/{version}.tar.gz'
         tools.get(url_.format(version=self.version))
-        os.rename("%s-%s"%(self.name, self.version), self.source_subfolder)
-        if self.is_msvc:
-            to_unicode(os.path.join(self.source_subfolder,'src/graphene-euler.c'))
+        if self.settings.os == 'Windows':
+            tools.patch(patch_file=self.patch)
+        os.rename("%s-%s"%(self.name, self.version), self._source_subfolder)
+        if self.settings.os == 'Windows':
+            to_unicode(os.path.join(self._source_subfolder,'src/graphene-euler.c'))
 
     def build(self):
-        defs = {'prefix':self.package_folder}
-
-
+        pkg_config_paths=[ os.path.join(self.deps_cpp_info[i].rootpath, "lib", "pkgconfig") for i in ["glib","libffi"] ]
+        prefix = os.path.join(self.build_folder, self._build_subfolder, "install")
+        defs = {'prefix':prefix, 'introspection':'false'}
+        if self.settings.os == "Linux":
+            defs.update({'libdir':'lib'})
         meson = Meson(self)
-        meson.configure( defs=defs,
-            source_dir=self.source_subfolder,
-            build_dir='_build')
+        meson.configure(defs=defs, source_dir = self._source_subfolder,
+                        build_dir=self._build_subfolder,pkg_config_paths=pkg_config_paths)
         meson.build()
-
         self.run('ninja -C {0} install'.format(meson.build_dir))
-        
 
+    #def gcc_build(self):
+    #    with tools.chdir(self._source_subfolder):
+    #        _args = ['--prefix=%s/builddir'%(os.getcwd()), '--libdir=%s/builddir/lib'%(os.getcwd()), '--disable-maintainer-mode',
+    #                 '--disable-silent-rules', '--disable-arm-neon']
+    #        if self.options.shared:
+    #            _args.extend(['--enable-shared=yes','--enable-static=no'])
+    #        else:
+    #            _args.extend(['--enable-shared=no','--enable-static=yes'])
 
-    def gcc_build(self):
-        with tools.chdir(self.source_subfolder):
-            _args = ['--prefix=%s/builddir'%(os.getcwd()), '--libdir=%s/builddir/lib'%(os.getcwd()), '--disable-maintainer-mode',
-                     '--disable-silent-rules', '--disable-arm-neon']
-            if self.options.shared:
-                _args.extend(['--enable-shared=yes','--enable-static=no'])
-            else:
-                _args.extend(['--enable-shared=no','--enable-static=yes'])
-
-            self.run('sh autogen.sh %s'%(' '.join(_args)))#space
-            self.run('make -j2')
-            self.run('make install')
+    #        self.run('sh autogen.sh %s'%(' '.join(_args)))#space
+    #        self.run('make -j2')
+    #        self.run('make install')
 
     def package(self):
-        if tools.os_info.is_linux:
-            with tools.chdir(self.source_subfolder):
-                self.copy("*", src="%s/builddir"%(os.getcwd()))
+        #if tools.os_info.is_linux:
+        #    with tools.chdir(self._source_subfolder):
+        #        self.copy("*", src="%s/builddir"%(os.getcwd()))
+        self.copy("*", dst=self.package_folder, src=os.path.join(self.build_folder,self._build_subfolder, "install"))
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
